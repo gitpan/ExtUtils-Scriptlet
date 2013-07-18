bcd32f5aa0165c3df7c0d8ea159d25ef5f36c932
Christian Walde <walde.christian@gmail.com>
2013-07-18 12:50:59 +0000

    Import of MITHALDU/ExtUtils-Scriptlet-1.131990 from CPAN.
    gitpan-cpan-distribution: ExtUtils-Scriptlet
    gitpan-cpan-version:      1.131990
    gitpan-cpan-path:         MITHALDU/ExtUtils-Scriptlet-1.131990.tar.gz
    gitpan-cpan-author:       MITHALDU
    gitpan-cpan-maturity:     released

--- lib/ExtUtils/Scriptlet.pm
+++ lib/ExtUtils/Scriptlet.pm
@@ -2,7 +2,7 @@ use strictures;
 
 package ExtUtils::Scriptlet;
 
-our $VERSION = '1.131980'; # VERSION
+our $VERSION = '1.131990'; # VERSION
 
 # ABSTRACT: run perl code in a new process without quoting it, on any OS
 
@@ -21,6 +21,7 @@ our $VERSION = '1.131980'; # VERSION
 
 use Exporter 'import';
 use autodie;
+use Data::Dumper;
 
 our @EXPORT_OK = qw( perl );
 
@@ -29,7 +30,15 @@ sub perl {
     my ( $code, %p ) = @_;
 
     die "No code given" if !$code;
-    die "\\r is not permitted in the code segment" if $code =~ /\r/;
+
+    # Serialize code to protect from newline mangling. This is necessary since
+    # the perl interpreter runs source code through a newline filter, no matter
+    # how the file handles are configured. For the payload this is unnecessary
+    # as no further filters are forced onto it.
+    # Using Data::Dumper here because on 5.10.0 B::perlstring breaks with utf8
+    # strings. if DD turns out to have more problems, this can be replaced with
+    # B::perlstring.
+    $code = Data::Dumper->new( [$code] )->Useqq( 1 )->Dump;
 
     $p{perl} ||= $^X;
     $p{encoding} ||= ":encoding(UTF-8)";
@@ -37,18 +46,23 @@ sub perl {
 
     open                                 #
       my $fh,                            #
-      "|- $p{encoding}",                 #
+      "|- :raw $p{encoding}",            # :raw protects the payload from write
+                                         # mangling (newlines)
       "$p{perl} $p{args} - $p{argv}";    #
 
     print $fh                            #
-      $code                              #
+      "binmode STDIN;"                   # protect the payload from read
+                                         # mangling (newlines, system locale)
+      . "$code; eval \$VAR1;"            # unpack and execute serialized code
+      . "die \$@ if \$@;"                #
       . "\n__END__\n"                    #
       . $p{payload};                     #
 
     eval {
+
         # prevent the host perl from being terminated if the child perl dies
         local $SIG{PIPE} = 'IGNORE';
-        close $fh;
+        close $fh;                       # grab exit value so we can return it
     };
 
     return $?;
@@ -65,7 +79,7 @@ ExtUtils::Scriptlet - run perl code in a new process without quoting it, on any 
 
 =head1 VERSION
 
-version 1.131980
+version 1.131990
 
 =head1 FUNCTIONS
 
